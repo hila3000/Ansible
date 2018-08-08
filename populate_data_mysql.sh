1dd7f8ad6335166848b612008a94213348782572
Hila Fish <hilavalensia@gmail.com>
2018-08-08 23:00:35 +0300

    Full stack with ELK,Cerebro,Nginx

--- populate_data_mysql.sh
+++ populate_data_mysql.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "drop table if exists"
-docker exec -it mysql_master mysql -uroot -pcomplexpass -D sample -e "DROP TABLE sample;"
+docker exec -it mysql_master mysql -uroot -pcomplexpass -D sample -e "DROP TABLE data;"
 
 echo "create table in sample DB"
-docker exec -it mysql_master mysql -uroot -pcomplexpass -D sample -e "create table data(a varchar(255));"
+docker exec -it mysql_master mysql -uroot -pcomplexpass -D sample -e "CREATE TABLE data(a varchar(255));"
 
 echo "Insert data into mysql"
-docker exec -it mysql_master mysql -uroot -pcomplexpass -D sample -e "insert into data set a=1;"
+docker exec -it mysql_master mysql -uroot -pcomplexpass -D sample -e "INSERT INTO data set a=1;"
 
 echo "check replication to slave - Doesn't work, needs fixing"
-docker exec -it mysql_slave mysql -uroot -pcomplexpass -D sample -e "select count(*) from data;"
+docker exec -it mysql_slave mysql -uroot -pcomplexpass -D sample -e "SELECT count(*) from data;"
 
